ca840245b2f1dcaf47c3deccb266aba564b5ea24
Harin <harin.lee@cellsignal.com>
2025-01-29 13:32:17 -0500

    Added more tests for pipeline CRUD operations

--- src/tests/test_schema_mutation.py
+++ src/tests/test_schema_mutation.py
@@ -4,6 +4,7 @@
 
 
 import pytest
+import json
 IN_DEV = True
 
 
@@ -153,7 +154,6 @@ class TestSchemaMutations:
     @pytest.mark.asyncio
     async def test_pipeline_with_config(self, mock_app, mock_info_context, mock_text_in, mock_text_out):
 
-        import json
         input_dict = {"type": "text.TextDataset", "filepath": str(mock_text_in)}
         output_dict = {"type": "text.TextDataset", "filepath": str(mock_text_out)}
         resp = await mock_app.schema.execute(self.mutation, 
@@ -175,7 +175,6 @@ class TestSchemaMutations:
     @pytest.mark.asyncio
     async def test_pipeline_with_data_catalog(self, mock_app, mock_info_context, mock_text_in, mock_text_out):
 
-        import json
         input_dict = {"type": "text.TextDataset", "filepath": str(mock_text_in)}
         output_dict = {"type": "text.TextDataset", "filepath": str(mock_text_out)}
         resp = await mock_app.schema.execute(self.mutation, 
@@ -194,8 +193,116 @@ class TestSchemaMutations:
     @pytest.mark.usefixtures('celery_session_worker')
     @pytest.mark.usefixtures('depends_on_current_app')
     @pytest.mark.asyncio
-    async def test_delete_pipeline(self, mock_app, mock_info_context, mock_text_in, mock_text_out):
-        
+    async def test_create_staged_pipeline(self, mock_app, mock_info_context, mock_text_in, mock_text_out):
+
+        create_pipeline_mutation = """
+        mutation TestMutation($pipeline: PipelineInput!) {
+          createPipeline(pipeline: $pipeline) {
+              id
+              name
+              status {
+                state
+                taskId
+              }
+            }
+          }
+        """
+
+        input_dict = {"type": "text.TextDataset", "filepath": str(mock_text_in)}
+        output_dict = {"type": "text.TextDataset", "filepath": str(mock_text_out)}
+        create_pipeline_resp = await mock_app.schema.execute(create_pipeline_mutation,
+                                                             variable_values={"pipeline": {
+                                                                 "name": "example00",
+                                                                 "dataCatalog": [{"name": "text_in", "config": json.dumps(input_dict)},
+                                                                                 {"name": "text_out", "config": json.dumps(output_dict)}],
+                                                                 "parameters": [{"name": "example", "value": "hello"},
+                                                                                {"name": "duration", "value": "0.1", "type": "FLOAT"}],
+                                                                 "state": "STAGED",
+                                                                 "tags": [{"key": "author", "value": "opensean"}, {"key": "package", "value": "kedro-graphql"}]
+                                                             }})
+
+        pipeline_state = create_pipeline_resp.data["createPipeline"]["status"][-1]["state"]
+
+        assert create_pipeline_resp.errors is None
+        assert pipeline_state == "STAGED"
+
+    @pytest.mark.usefixtures('mock_celery_session_app')
+    @pytest.mark.usefixtures('celery_session_worker')
+    @pytest.mark.usefixtures('depends_on_current_app')
+    @pytest.mark.asyncio
+    async def test_create_valid_ready_pipeline(self, mock_app, mock_info_context, mock_text_in, mock_text_out):
+
+        create_pipeline_mutation = """
+        mutation TestMutation($pipeline: PipelineInput!) {
+          createPipeline(pipeline: $pipeline) {
+              id
+              name
+              status {
+                state
+                taskId
+              }
+            }
+          }
+        """
+
+        input_dict = {"type": "text.TextDataset", "filepath": str(mock_text_in)}
+        output_dict = {"type": "text.TextDataset", "filepath": str(mock_text_out)}
+        create_pipeline_resp = await mock_app.schema.execute(create_pipeline_mutation,
+                                                             variable_values={"pipeline": {
+                                                                 "name": "example00",
+                                                                 "dataCatalog": [{"name": "text_in", "config": json.dumps(input_dict)},
+                                                                                 {"name": "text_out", "config": json.dumps(output_dict)}],
+                                                                 "parameters": [{"name": "example", "value": "hello"},
+                                                                                {"name": "duration", "value": "0.1", "type": "FLOAT"}],
+                                                                 "state": "READY",
+                                                                 "tags": [{"key": "author", "value": "opensean"}, {"key": "package", "value": "kedro-graphql"}]
+                                                             }})
+
+        pipeline_state = create_pipeline_resp.data["createPipeline"]["status"][-1]["state"]
+
+        assert create_pipeline_resp.errors is None
+        assert pipeline_state != "STAGED"
+
+    @pytest.mark.usefixtures('mock_celery_session_app')
+    @pytest.mark.usefixtures('celery_session_worker')
+    @pytest.mark.usefixtures('depends_on_current_app')
+    @pytest.mark.asyncio
+    async def test_create_invalid_name_ready_pipeline(self, mock_app, mock_info_context, mock_text_in, mock_text_out):
+
+        create_pipeline_mutation = """
+        mutation TestMutation($pipeline: PipelineInput!) {
+          createPipeline(pipeline: $pipeline) {
+              id
+              name
+              status {
+                state
+                taskId
+              }
+            }
+          }
+        """
+
+        input_dict = {"type": "text.TextDataset", "filepath": str(mock_text_in)}
+        output_dict = {"type": "text.TextDataset", "filepath": str(mock_text_out)}
+        create_pipeline_resp = await mock_app.schema.execute(create_pipeline_mutation,
+                                                             variable_values={"pipeline": {
+                                                                 "name": "example01",
+                                                                 "dataCatalog": [{"name": "text_in", "config": json.dumps(input_dict)},
+                                                                                 {"name": "text_out", "config": json.dumps(output_dict)}],
+                                                                 "parameters": [{"name": "example", "value": "hello"},
+                                                                                {"name": "duration", "value": "0.1", "type": "FLOAT"}],
+                                                                 "state": "READY",
+                                                                 "tags": [{"key": "author", "value": "opensean"}, {"key": "package", "value": "kedro-graphql"}]
+                                                             }})
+
+        assert create_pipeline_resp.errors is not None
+
+    @pytest.mark.usefixtures('mock_celery_session_app')
+    @pytest.mark.usefixtures('celery_session_worker')
+    @pytest.mark.usefixtures('depends_on_current_app')
+    @pytest.mark.asyncio
+    async def test_update_pipeline_staged_to_ready(self, mock_app, mock_info_context, mock_text_in, mock_text_out):
+
         create_pipeline_mutation = """
         mutation TestMutation($pipeline: PipelineInput!) {
           createPipeline(pipeline: $pipeline) {
@@ -204,16 +311,74 @@ class TestSchemaMutations:
         }
         """
 
-        create_pipeline_resp = await mock_app.schema.execute(create_pipeline_mutation, 
-                                    variable_values = {"pipeline": {
-                                      "name": "example00",
-                                      "inputs": [{"name": "text_in", "type": "text.TextDataset", "filepath": str(mock_text_in)}],
-                                      "outputs": [{"name": "text_out", "type": "text.TextDataset", "filepath": str(mock_text_out)}],
-                                      "parameters": [{"name":"example", "value":"hello"},
-                                                     {"name": "duration", "value": "0.1", "type": "FLOAT"}],
-                                      "tags": [{"key": "author", "value": "opensean"},{"key":"package", "value":"kedro-graphql"}]
-                                    }})
-        
+
+        input_dict = {"type": "text.TextDataset", "filepath": str(mock_text_in)}
+        output_dict = {"type": "text.TextDataset", "filepath": str(mock_text_out)}
+
+        create_pipeline_resp = await mock_app.schema.execute(create_pipeline_mutation,
+                                                             variable_values={"pipeline": {
+                                                                 "name": "example00",
+                                                                 "dataCatalog": [{"name": "text_in", "config": json.dumps(input_dict)},
+                                                                                 {"name": "text_out", "config": json.dumps(output_dict)}],
+                                                                 "parameters": [{"name": "example", "value": "hello"},
+                                                                                {"name": "duration", "value": "0.1", "type": "FLOAT"}],
+                                                                 "state": "STAGED",
+                                                                 "tags": [{"key": "author", "value": "opensean"}, {"key": "package", "value": "kedro-graphql"}]
+                                                             }})
+
+        pipeline_id = create_pipeline_resp.data["createPipeline"]["id"]
+
+        update_pipeline_mutation = """
+        mutation TestMutation($pipeline: PipelineInput!, $id: String!) {
+          updatePipeline(pipeline: $pipeline, id: $id) {
+          id
+          name
+          status {
+            state
+            taskId
+          }
+          }
+        }
+        """
+
+        update_pipeline_resp = await mock_app.schema.execute(update_pipeline_mutation,
+                                                             variable_values={"id": pipeline_id,
+                                                                              "pipeline": {
+                                                                                  "name": "example00",
+                                                                                  "state": "READY",
+                                                                              }
+                                                                              })
+        assert update_pipeline_resp.errors is None
+        pipeline_state = update_pipeline_resp.data["updatePipeline"]["status"][-1]["state"]
+        assert pipeline_state != "STAGED"
+
+    @pytest.mark.usefixtures('mock_celery_session_app')
+    @pytest.mark.usefixtures('celery_session_worker')
+    @pytest.mark.usefixtures('depends_on_current_app')
+    @pytest.mark.asyncio
+    async def test_delete_pipeline(self, mock_app, mock_info_context, mock_text_in, mock_text_out):
+
+        create_pipeline_mutation = """
+        mutation TestMutation($pipeline: PipelineInput!) {
+          createPipeline(pipeline: $pipeline) {
+          id
+          }
+        }
+        """
+
+        input_dict = {"type": "text.TextDataset", "filepath": str(mock_text_in)}
+        output_dict = {"type": "text.TextDataset", "filepath": str(mock_text_out)}
+
+        create_pipeline_resp = await mock_app.schema.execute(create_pipeline_mutation,
+                                                             variable_values={"pipeline": {
+                                                                 "name": "example00",
+                                                                 "dataCatalog": [{"name": "text_in", "config": json.dumps(input_dict)},
+                                                                                 {"name": "text_out", "config": json.dumps(output_dict)}],
+                                                                 "parameters": [{"name": "example", "value": "hello"},
+                                                                                {"name": "duration", "value": "0.1", "type": "FLOAT"}],
+                                                                 "tags": [{"key": "author", "value": "opensean"}, {"key": "package", "value": "kedro-graphql"}]
+                                                             }})
+
         pipeline_id = create_pipeline_resp.data["createPipeline"]["id"]
 
         delete_pipeline_mutation = """
@@ -224,8 +389,8 @@ class TestSchemaMutations:
         }
         """
 
-        delete_pipeline_resp = await mock_app.schema.execute(delete_pipeline_mutation, 
-                                    variable_values = {"id": pipeline_id})
+        delete_pipeline_resp = await mock_app.schema.execute(delete_pipeline_mutation,
+                                                             variable_values={"id": pipeline_id})
 
         assert delete_pipeline_resp.errors is None
     
